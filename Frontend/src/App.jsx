e6da769d517697fdb153c7dc580689201aa3beec
Abuzar <abuzar0561446@gmail.com>
2024-07-03 10:53:42 +0530

    Set up frontend with React using vite, Installed and configured Tailwind CSS for utility-first styling.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+  return (
+    <div className=''>
+    <p className='text-bold text-red-500 bg-blue-400 text-center'>Company Bench Sharing Portal
+      </p>  
+    </div>
+  )
+}
+
+export default App
