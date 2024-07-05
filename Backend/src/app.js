8308e7893bc81874be0d87249502b5d1fc5ecf7d
Abuzar <abuzar0561446@gmail.com>
2024-07-05 22:07:24 +0530

    fix cors origin error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app =express();
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,
+//     credentials:true
+// }))
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+    origin: 'https://abuzar-bench-portal.netlify.app/',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
+
+
 app.use(express.json({ limit: "16kb" })); 
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // for url  encoded data
 app.use(express.static("public"));  // to serve static files css js
